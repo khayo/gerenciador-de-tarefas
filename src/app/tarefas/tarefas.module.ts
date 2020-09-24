abcae841b1a152f89494b067f5f127fb95a8259e
khayo <khayovannucci@gmail.com>
2020-09-23 21:17:59 -0300

    Criado modulo de rotas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TarefaService } from "./shared";
+import { ListarTarefaComponent } from './listar';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [
     CommonModule
   ],
